1f99e3c8f7e64edcc001126160c02bfbe816085a
Deceevn <deceevn@gmail.com>
2016-08-31 04:05:04 +0000

    first commit

---
+++ flux-inventory/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+  <h1>Flux Inventory</h1>
+    <list></list>
+  </div>
+</template>
+
+<script>
+import List from './components/List'
+
+export default {
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-size: 18px;
+  font-family: 'Open Sans Condensed', sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+#app h1,h2,h3,h4,h5,h6 {
+    font-family: 'Raleway', sans-serif;
+    font-size: 26px;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ flux-inventory/src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="inv-list">
+    <ul>
+        <li v-for="item in invItems"><i class="fa fa-square-o" aria-hidden="true"></i> {{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      invItems: ['Something', 'Other', 'Things']
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+
+li {
+  text-align: left;
+  list-style: none;
+}
+
+li .fa {
+    margin-right: 5px;
+}
+</style>
